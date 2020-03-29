d48407722622b76576c1e813353bb2e12805ec4b
Micah McCallum <micahmccallum@outlook.com>
2020-03-29 02:51:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,7 @@ def adding_to_dennis
         }
      }
 
-		 programmer_hash[:dennis_ritchie][:known_for][1] == "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 programmer_hash
 
 end
