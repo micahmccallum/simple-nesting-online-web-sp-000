4c0b4221200233e92b85b4a06e3014481fb55979
Micah McCallum <micahmccallum@outlook.com>
2020-03-29 01:55:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
+	programmer_hash[:alan_kay][:alan_kay_is_known_for]
 	programmer_hash =
  		{
         :grace_hopper => {
